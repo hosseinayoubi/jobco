454c2f9a765e30abec51d5b6016ff9f9e2b9e3bb
Abdolhossein Ayoubi <125464377+hosseinayoubi@users.noreply.github.com>
2026-03-01 23:30:09 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -107,7 +107,3 @@ export default {
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
-
-
-
-================================================
