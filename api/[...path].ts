da840df127b0cd7ab19922db3c1e0db183302a21
Abdolhossein Ayoubi <125464377+hosseinayoubi@users.noreply.github.com>
2026-03-03 18:30:11 +0200

    Update [...path].ts

--- api/[...path].ts
+++ api/[...path].ts
@@ -2,8 +2,5 @@ import serverless from "serverless-http";
 import { createApp } from "../server/app";
 
 const app = createApp();
-const handler = serverless(app);
 
-export default async function (req: any, res: any) {
-  await handler(req, res);
-}
+export default serverless(app);
