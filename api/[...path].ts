1cc9374398ba4a30ce16121e392e98d75e89db86
Abdolhossein Ayoubi <125464377+hosseinayoubi@users.noreply.github.com>
2026-03-04 03:31:40 +0200

    Update [...path].ts

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,6 +1,10 @@
 import serverless from "serverless-http";
 import { createApp } from "../server/app";
 
-const app = createApp();
+// createApp is now async (because registerRoutes is async)
+const appPromise = createApp();
 
-export default serverless(app);
+export default async function handler(req: any, res: any) {
+  const app = await appPromise;
+  return serverless(app)(req, res);
+}
